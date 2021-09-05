1c94d47b1ced72282fa3cb747dbd2b584624a55c
wwy38645464 <wwy38645464@163.com>
2021-09-05 21:42:35 +0800

    this is initial version

---
+++ charfreq.js
@@ -0,0 +1,67 @@
+class DefaultMap extends Map{
+    constructor(defaultValue){
+        super();
+        this.defaultValue = defaultValue;
+    }
+
+    get(key) {
+        if (this.has(key)){
+            return super.get(key);
+        }
+        else{
+            return this.defaultValue;
+        }
+    }
+}
+
+//柱状图显示
+
+class Histogram{
+    constructor(){
+        this.letterCounts = new DefaultMap(0);
+        this.totalLetter = 0;
+    }
+
+    add(text){
+        text = text.replace(/\s/g, "").toUpperCase();
+
+        for(let character of text){
+            let count = this.letterCounts.get(character); //取得之前的数量
+            this.totalLetter++;
+        }
+    }
+
+    toString() {
+        let entries = [...this.letterCounts];
+        entries.sort((a,b) => {
+            if(a[1] === b[1]){
+                return a[0] < b[0] ? -1 : 1;
+            }else{
+                return b[1] - a[1];
+            }
+        });
+        
+        for(let entry of entries){
+            entry[1] = entry[1] / this.totalLetter * 100;
+        }
+
+        entries = entries.filter(entry => entry[1] >= 1);
+
+        let lines = entries.map(
+            ([l,n]) => `${l}: ${"#".repeat(Math.round(n))} ${n.toFixed(2)}%`
+        );
+
+        return lines.join("\n");
+    }
+}
+
+async function histogramFromStdin(){
+    process.stdin.setEncoding("utf-8");
+    let histogram = new Histogram();
+    for await (let chunk of process.stdin){
+        histogram.add(chunk);
+    }
+    return histogram;
+}
+
+histogramFromStdin().then(histogram => {console.log(histogram.toString()); });
